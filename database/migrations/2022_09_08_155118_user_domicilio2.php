c80fe7bece30ec396365bacde3ae088c9066c424
acrescodev <acresco.dev@gmail.com>
2022-09-08 10:55:18 -0500

    TEST MIGRACION SE AGREGO EL CAMPO DE FECHA DE NACIMIENTO EN LA TABLA USER DOMICILIO

---
+++ database/migrations/2022_09_08_155118_user_domicilio2.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserDomicilio2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->string('domicilio');
+            $table->string('numero_exterior');
+            $table->string('colonia');
+            $table->string('cp');
+            $table->string('ciudad');
+            $table->date('fecha_nacimento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+}
